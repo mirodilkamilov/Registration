38fd3ce8b7129268c07b0b5072c1dbf31653f1af
mirodil1999 <mirodilkamilov1999@gmail.com>
2020-08-21 16:54:01 +0500

    dummy commit

--- index.php
+++ index.php
@@ -6,69 +6,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/v4-shims.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
     <title>Document</title>
 </head>
 
 <body>
     <div class="container" id="container">
-        <div class="form-container sign-up-container">
-            <form action="#">
-                <h1>Create Account</h1>
-                <div class="social-container">
-                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-                <span>or use your email for registration</span>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button>Sign Up</button>
-            </form>
-        </div>
         <div class="form-container sign-in-container">
             <form action="#">
-                <h1>Sign in</h1>
-                <div class="social-container">
-                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                <h1>Login</h1>
+                <p class="form-paragraph">Sign in to your account to continue.</p>
+                <div class="input-area">
+                    <input type="text" required />
+                    <span class="floating-labels">Email address</span>
                 </div>
-                <span>or use your account</span>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <a href="#">Forgot your password?</a>
-                <button>Sign In</button>
+                <div class="input-area">
+                    <input type="password" required />
+                    <span class="floating-labels">Password</span>
+                </div>
+                <a class="form-question" href="">Forgot your password?</a>
+                <button class="form-button sign-in-button" type="submit" value="sign in">Sign In</button>
+                <div class="form-footer">
+                    <p class="form-paragraph">Not registered? <button type="button" class="footer-btn" id="create-account-link">Create account</button></p>
+                </div>
             </form>
         </div>
-        <div class="overlay-container">
-            <div class="overlay">
-                <div class="overlay-panel overlay-left">
-                    <h1>Welcome Back!</h1>
-                    <p>To keep connected with us please login with your personal info</p>
-                    <button class="ghost" id="signIn">Sign In</button>
+        <div class="form-container sign-up-container">
+            <form action="#">
+                <h1>Register</h1>
+                <p class="form-paragraph">Create account to continue.</p>
+                <div class="input-area">
+                    <input type="text" required />
+                    <span class="floating-labels">First name</span>
                 </div>
-                <div class="overlay-panel overlay-right">
-                    <h1>Hello, Friend!</h1>
-                    <p>Enter your personal details and start journey with us</p>
-                    <button class="ghost" id="signUp">Sign Up</button>
+                <div class="input-area">
+                    <input type="text" required />
+                    <span class="floating-labels">Last name</span>
                 </div>
-            </div>
+                <div class="input-area">
+                    <input type="password" required />
+                    <span class="floating-labels">Password</span>
+                </div>
+                <button class="form-button sign-up-button" type="submit" value="sign up">Sign Up</button>
+                <div class="form-footer">
+                    <p class="form-paragraph">Already have an account? <button type="button" class="footer-btn" id="sign-in-link">Sign in</button></p>
+                </div>
+            </form>
         </div>
     </div>
 
     <script>
-        const signUpButton = document.getElementById('signUp');
-        const signInButton = document.getElementById('signIn');
-        const container = document.getElementById('container');
+        const createAccountLink = document.getElementById('create-account-link');
+        const signInLink = document.getElementById('sign-in-link');
+        const formContainer = document.getElementById('container');
 
-        signUpButton.addEventListener('click', () => {
-            container.classList.add("right-panel-active");
+        createAccountLink.addEventListener('click', () => {
+            formContainer.classList.add('container-active');
         });
 
-        signInButton.addEventListener('click', () => {
-            container.classList.remove("right-panel-active");
+        signInLink.addEventListener('click', () => {
+            formContainer.classList.remove('container-active');
         });
     </script>
 </body>
