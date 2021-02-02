486df848d8139640c653bad14ffd2744f1e92b8b
mirodil1999 <mirodilkamilov1999@gmail.com>
2021-02-02 20:40:30 +0500

    database setup for deployement

--- classes/Connection.php
+++ classes/Connection.php
@@ -6,10 +6,18 @@ class Connection
 
     protected function connect()
     {
-        $servername = "localhost";
-        $username = "root";
-        $password = "root";
-        $dbname = "registration";
+        $url = getenv('JAWSDB_NAVY_URL');
+        $dbparts = parse_url($url);
+
+        $servername = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $dbname = ltrim($dbparts['path'], '/');
+
+        // $servername = "localhost";
+        // $username = "root";
+        // $password = "root";
+        // $dbname = "registration";
         $this->pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
